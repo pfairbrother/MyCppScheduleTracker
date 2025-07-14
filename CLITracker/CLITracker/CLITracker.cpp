5bc4f9599afd2b224d390c50ec2630d731c47ad3
Peter Fairbrother <fairbrother.peter@gmail.com>
2025-07-14 17:28:02 -0600

    implemented task struct, core logic for adding new tasks, viewing current tasks and marking them completed, and viewing completed tasks.

--- CLITracker/CLITracker/CLITracker.cpp
+++ CLITracker/CLITracker/CLITracker.cpp
@@ -2,10 +2,102 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <limits>
+#include "Task.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::vector<Task> tasks;
+    std::vector<Task> completeTasks;
+
+    char select;
+
+    do {
+        std::cout << "Welcome to the Task Tracker!\nPress 1 to view current tasks.\nPress 2 to view completed tasks\nPress 3 to add tasks.\nPress 4 to quit." << std::endl;
+        std::cin >> select;
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        if (select == '1') {
+            char choice;
+            do {
+                if (tasks.empty()) {
+                    std::cout << std::endl << "No tasks currently in list" << std::endl;
+                }
+                else {
+                    char mark;
+                    do {
+                        std::cout << "\n--- Current Tasks ---" << std::endl;
+                        for (size_t i = 0; i < tasks.size(); ++i) {
+                            std::cout << (i + 1) << ". " << "Description: " << tasks[i].description << ", Due Date:" << tasks[i].dueDate << std::endl;
+                        }
+                        std::cout << "------------------------------------" << std::endl;
+                        std::cout << "Input task number to mark complete. E to exit." << std::endl;
+                        std::cin >> mark;
+                        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                        if (mark >= '1' && mark <= '9') {
+                            int taskNumber = static_cast<int>(mark - '0') - 1;
+                            if (taskNumber >= 0 && taskNumber < tasks.size()) {
+                                completeTasks.push_back(tasks[taskNumber]);
+                                tasks.erase(tasks.begin() + taskNumber);
+                                std::cout << "Task Completed!" << std::endl;
+                            }
+                            else {
+                                std::cout << "Invalid Number. Try again." << std::endl;
+                            }
+                        }
+                    } while (mark != 'e' && mark != 'E' && tasks.size() > 0);
+                }
+                std::cout << "Back to menu? (Y/N): " << std::endl;
+                std::cin >> choice;
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            }while (choice == 'n' || choice == 'N');
+        }
+        else if (select == '2') {
+            char choice;
+            if (completeTasks.empty()) {
+                std::cout << "No tasks completed.\n";
+            }
+            else {
+                std::cout << "\n--- Completed Tasks ---" << std::endl;
+                for (size_t i = 0; i < completeTasks.size(); ++i) {
+                    std::cout << (i + 1) << ". " << "Description: " << completeTasks[i].description << ", Due Date:" << completeTasks[i].dueDate << std::endl;
+                }
+                std::cout << "------------------------------------" << std::endl;
+            }
+            std::cout << "Back to menu? (Y/N): " << std::endl;
+            std::cin >> choice;
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+        else if (select == '3') {
+            char choice;
+            do {
+                std::string tempDescription;
+                std::string tempDueDate;
+
+                std::cout << std::endl << "Please enter a task description: " << std::endl;
+                std::getline(std::cin, tempDescription);
+
+                std::cout << "Please enter a due date (use YYYY-MM-DD): " << std::endl;
+                std::cin >> tempDueDate;
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+                Task newTask;
+                newTask.description = tempDescription;
+                newTask.dueDate = tempDueDate;
+
+
+                tasks.push_back(newTask);
+                std::cout << "Task added! Add another? (Y/N): ";
+                std::cin >> choice;
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+            } while (choice == 'y' || choice == 'Y');
+        }
+
+    } while (select != '4');
+
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
---
+++ CLITracker/CLITracker/Task.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <string>
+
+struct Task {
+	std::string description;
+	std::string dueDate;
+};
